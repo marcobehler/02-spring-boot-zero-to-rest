4635dc3265e75f2d211cdabfc2798fff35a2ce17
Marco Behler <marco@marcobehler.com>
2017-01-12 18:49:36 +0100

    initial

--- src/main/java/com/marcobehler/ShoppingController.java
+++ src/main/java/com/marcobehler/ShoppingController.java
@@ -24,9 +24,17 @@ import java.util.List;
 @RestController
 public class ShoppingController {
 
+
+    @RequestMapping(value = "/basket/update", method = RequestMethod.POST)
+    public String updateBasket(@RequestParam String name, @RequestParam Integer id, @RequestParam @DateTimeFormat(pattern = "dd.MM.yyyy") Date added) {
+        // fake!!!
+        return "Item [" + id + " , " + name + " , " + added + " ] got added to the shopping basket!";
+    }
+
+
     @RequestMapping(value = "/searchItems", method = RequestMethod.GET)
     public List<Item> searchItems(@RequestParam(required = false) String name, @RequestParam(required = false) Integer id,
-                            @RequestParam(required = false) @DateTimeFormat(pattern = "dd.MM.yyyy") Date added) {
+                                  @RequestParam(required = false) @DateTimeFormat(pattern = "dd.MM.yyyy") Date added) {
         // fake!
         return Arrays.asList(new Item(name, id, added));
     }
